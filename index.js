681e51f606fb690a941c9d9bdf16c397283d1b55
Your_Name <joseph161216@gmail.com>
2019-12-10 10:13:34 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const textEle = document.querySelector("#text")
+console.log(textEle)
+// console.log("L3")
+textEle.textContent =
+  "JavaScript is so cool. It lets me add text to my page programmatically.";
+// console.log("L4");
+
+document.addEventListener("DOMContentLoaded", function() {
+  // console.log(
+  //   "JavaScript is so cool. It lets me add text to my page programmatically."
+  // );
+  textEle.textContent = "This is really cool!";
+});
+// console.log(
+//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// );
